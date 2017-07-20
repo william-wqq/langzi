46b771779ecccf4e64b027a78e3d47610d20f8a8
william <william_wqq@163.com>
2017-07-20 16:02:58 +0800

    add a class

---
+++ src/ArrayClass.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: william
+ * Date: 2017/7/20
+ * Time: 16:01
+ */
+
+namespace William\Arr;
+
+
+class ArrayClass
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        //
+    }
+
+}
